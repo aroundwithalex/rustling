a9b35993b45e2681a10901e63acaee003c21aa5a
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2022-11-13 16:41:14 +0000

    Add coin enum

---
+++ projects/coins/src/main.rs
@@ -0,0 +1,49 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+#[derive(Debug)]
+enum Either {
+    Left(usize),
+    Right(String)
+}
+
+enum Location {
+    Point(i32),
+    Range(i32, i32)
+}
+
+fn main() {
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let l: Location = Location::Range(0, 5);
+    let n = match l {
+        Location::Point(_) => -1,
+        Location::Range(_, n) => n,
+        Location::Range(0, _) => 0,
+        _ => -2
+    };
+    println!("{n}");
+}
